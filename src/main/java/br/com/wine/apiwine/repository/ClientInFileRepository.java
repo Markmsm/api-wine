6d35fa80c5be86b8c4928c588d95824538ab14f3
Mark <marksmunari@gmail.com>
2021-11-08 08:28:57 -0300

    refactor ClientInFileRepository

--- src/main/java/br/com/wine/apiwine/repository/ClientInFileRepository.java
+++ src/main/java/br/com/wine/apiwine/repository/ClientInFileRepository.java
@@ -10,30 +10,29 @@ import java.util.Scanner;
 
 public class ClientInFileRepository implements ClientRepository{
 
+    Scanner reader;
+
+    public ClientInFileRepository(Scanner reader) {
+        this.reader = reader;
+    }
+
     @Override
     public List<Client> getAll() {
         List<Client> clients = new ArrayList<>();
 
-        try {
-            File clientsCSV = new File("/home/ubots/Learnspace/api-wine/src/main/files/clients.csv");
-            Scanner reader = new Scanner(clientsCSV);
-            reader.nextLine();
+        reader.nextLine();
 
-            while (reader.hasNextLine()) {
-                String[] clientInfos = reader.nextLine().split(",");
+        while (reader.hasNextLine()) {
+            String[] clientInfos = reader.nextLine().split(",");
 
-                int clientId = Integer.parseInt(clientInfos[0]);
-                String clientName = clientInfos[1];
-                String clientCpf = clientInfos[2];
+            int clientId = Integer.parseInt(clientInfos[0]);
+            String clientName = clientInfos[1];
+            String clientCpf = clientInfos[2];
 
-                clients.add(new Client(clientId, clientName, clientCpf));
-            }
-
-            reader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Deu ruim!!");
-            e.printStackTrace();
+            clients.add(new Client(clientId, clientName, clientCpf));
         }
+        reader.close();
+
         return clients;
     }
 }

ceb25b9bd59e90d363b2012fa122b6fdaf0e8747
Mark <marksmunari@gmail.com>
2021-11-09 14:32:18 -0300

    add CpfFormatter class

--- src/main/java/br/com/wine/apiwine/service/CpfFormatter.java
+++ src/main/java/br/com/wine/apiwine/service/CpfFormatter.java
@@ -1,9 +1,11 @@
 package br.com.wine.apiwine.service;
 
-public class cpfFormatter {
+public class CpfFormatter {
 
     public String formatCpf(String cpf) {
         cpf = cpf.replaceAll("\\.|-", "");
+
+        if (!cpf.matches("^[0-9]*$")) throw new NumberFormatException("This cpf is not valid!");
 
         if (cpf.length() != 11) cpf = cpf.substring(cpf.length() - 11);
 
--- src/test/java/br/com/wine/apiwine/service/CpfFormatterTest.java
+++ src/test/java/br/com/wine/apiwine/service/CpfFormatterTest.java
@@ -1,2 +1,50 @@
-package br.com.wine.apiwine.service;public class CpfFormatterTest {
+package br.com.wine.apiwine.service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CpfFormatterTest {
+
+    private static CpfFormatter cpfFormatter;
+
+    @BeforeAll
+    static void setUp() {
+        cpfFormatter = new CpfFormatter();
+    }
+
+    @Test
+    void formatCpfTesteeera() {
+        Throwable ex = assertThrows(NumberFormatException.class, () -> {
+            String cpf = cpfFormatter.formatCpf("testing cpf formatter");
+        });
+
+        assertEquals("This cpf is not valid!", ex.getMessage());
+    }
+
+    @Test
+    void formatCpfShouldFormatWithMoreThanElevenChar() {
+        String expectedCpf = "11122233344";
+        String cpf = cpfFormatter.formatCpf("0000000011122233344");
+
+        assertEquals(expectedCpf, cpf);
+    }
+
+    @Test
+    void formatCpfShouldFormatRemovingDots() {
+        String expectedCpf = "11122233344";
+        String cpf = cpfFormatter.formatCpf("111.222.333.44");
+
+        assertEquals(expectedCpf, cpf);
+    }
+
+    @Test
+    void formatCpfShouldFormatRemovingHyphen() {
+        String expectdCpf = "11122233344";
+        String cpf = cpfFormatter.formatCpf("111-222-333-44");
+
+        assertEquals(expectdCpf, cpf);
+    }
 }

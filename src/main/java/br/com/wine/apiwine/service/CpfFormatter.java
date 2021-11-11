80628e707fa38af9ee35c72ffb49993afef51957
Mark <marksmunari@gmail.com>
2021-11-11 08:29:04 -0300

    refactor cpf formatter

--- src/main/java/br/com/wine/apiwine/service/CpfFormatter.java
+++ src/main/java/br/com/wine/apiwine/service/CpfFormatter.java
@@ -2,13 +2,13 @@ package br.com.wine.apiwine.service;
 
 public class CpfFormatter {
 
-    public String formatCpf(String cpf) {
-        cpf = cpf.replaceAll("\\.|-", "");
+    static public String formatCpf(String cpf) {
+        String formattedCpf = cpf.replaceAll("\\.|-", "");
 
-        if (!cpf.matches("^[0-9]*$")) throw new NumberFormatException("This cpf is not valid!");
+        if (!formattedCpf.matches("^[0-9]*$")) throw new NumberFormatException("This cpf is not valid!");
 
-        if (cpf.length() != 11) cpf = cpf.substring(cpf.length() - 11);
+        if (formattedCpf.length() != 11) formattedCpf = formattedCpf.substring(cpf.length() - 11);
 
-        return cpf;
+        return formattedCpf;
     }
 }
--- src/test/java/br/com/wine/apiwine/service/CpfFormatterTest.java
+++ src/test/java/br/com/wine/apiwine/service/CpfFormatterTest.java
@@ -1,50 +1,60 @@
 package br.com.wine.apiwine.service;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static br.com.wine.apiwine.service.CpfFormatter.formatCpf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CpfFormatterTest {
 
-    private static CpfFormatter cpfFormatter;
-
-    @BeforeAll
-    static void setUp() {
-        cpfFormatter = new CpfFormatter();
-    }
+    //somente pontos
+    //passando null
 
     @Test
     void formatCpfTesteeera() {
+        //When:
         Throwable ex = assertThrows(NumberFormatException.class, () -> {
-            String cpf = cpfFormatter.formatCpf("testing cpf formatter");
+            String cpf = formatCpf("testing cpf formatter");
         });
 
+        //Then:
         assertEquals("This cpf is not valid!", ex.getMessage());
     }
 
     @Test
     void formatCpfShouldFormatWithMoreThanElevenChar() {
+        //Given:
         String expectedCpf = "11122233344";
-        String cpf = cpfFormatter.formatCpf("0000000011122233344");
 
+        //When:
+        String cpf = formatCpf("0000000011122233344");
+
+        //Then:
         assertEquals(expectedCpf, cpf);
     }
 
     @Test
     void formatCpfShouldFormatRemovingDots() {
+        //Given:
         String expectedCpf = "11122233344";
-        String cpf = cpfFormatter.formatCpf("111.222.333.44");
 
+        //When:
+        String cpf = formatCpf("111.222.333.44");
+
+        //Then:
         assertEquals(expectedCpf, cpf);
     }
 
     @Test
     void formatCpfShouldFormatRemovingHyphen() {
+        //Given:
         String expectdCpf = "11122233344";
-        String cpf = cpfFormatter.formatCpf("111-222-333-44");
 
+        //When:
+        String cpf = formatCpf("111-222-333-44");
+
+        //Then:
         assertEquals(expectdCpf, cpf);
     }
 }

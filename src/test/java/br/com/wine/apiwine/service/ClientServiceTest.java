1c7dde5234287a6d316eef63fe2beac11afd00e3
Mark <marksmunari@gmail.com>
2021-11-11 08:14:42 -0300

    refactor ClientServiceTest

--- src/test/java/br/com/wine/apiwine/service/ClientServiceTest.java
+++ src/test/java/br/com/wine/apiwine/service/ClientServiceTest.java
@@ -2,13 +2,14 @@ package br.com.wine.apiwine.service;
 
 import br.com.wine.apiwine.data.model.*;
 import br.com.wine.apiwine.repository.ClientRepository;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
+import static br.com.wine.apiwine.data.model.ClientCreator.*;
+import static br.com.wine.apiwine.data.model.PurchaseCreator.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -18,62 +19,79 @@ public class ClientServiceTest {
     static ClientRepository mockedClientRepository;
     static PurchaseService mockedPurchaseService;
     static ClientService clientService;
-    static PurchaseCreator purchaseCreator;
-    static ClientCreator clientCreator;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         mockedClientRepository = mock(ClientRepository.class);
         mockedPurchaseService = mock(PurchaseService.class);
         clientService = new ClientService(mockedClientRepository, mockedPurchaseService);
-        purchaseCreator = new PurchaseCreator();
-        clientCreator = new ClientCreator();
-
-        when(mockedClientRepository.getAll()).thenReturn(clientCreator.getFakeClients());
-        when(mockedPurchaseService.getAll()).thenReturn(purchaseCreator.getFakePurchases());
     }
 
     @Test
     void getAllShouldThrowExceptionIfNoClient() {
-        when(mockedClientRepository.getAll()).thenReturn(clientCreator.getFakeClientsEmpty());
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClientsEmpty());
 
+        //When:
         Throwable ex = assertThrows(NoSuchElementException.class, () -> {
             List<Client> clients = clientService.getAll();
         });
 
+        //Then:
         assertEquals("There is no client!", ex.getMessage());
     }
 
     @Test
     void getAllShouldReturnClients() {
-        List<Client> expectedClients = clientCreator.getFakeClients();
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClients());
+        List<Client> expectedClients = getFakeClients();
+
+        //When:
         List<Client> clients = clientService.getAll();
 
+        //Then:
         assertEquals(expectedClients.size(), clients.size());
         assertTrue(clients.containsAll(expectedClients));
     }
 
     @Test
     void getClientsSortedByMaxSpentShouldReturnNoClientIfNotExistClient() {
-        when(mockedClientRepository.getAll()).thenReturn(clientCreator.getFakeClientsEmpty());
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClientsEmpty());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchases());
+
+        //When:
         List<Client> clients = clientService.getClientsSortedByMaxSpent();
 
+        //Then:
         assertTrue(clients.isEmpty());
     }
 
     @Test
     void getClientsSortedByMaxSpentShouldReturnNoClientIfNotExistPurchase() {
-        when(mockedPurchaseService.getAll()).thenReturn(purchaseCreator.getFakePurchasesEmpty());
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClients());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchasesEmpty());
+
+        //When:
         List<Client> clients = clientService.getClientsSortedByMaxSpent();
 
+        //Then:
         assertTrue(clients.isEmpty());
     }
 
     @Test
     void getClientsSortedByMaxSpentShouldReturnClientsWithoutSpentInLastPosition() {
-        List<Client> expectedClients = clientCreator.getFakeClients();
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClients());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchases());
+        List<Client> expectedClients = getFakeClients();
+
+        //When:
         List<Client> clients = clientService.getClientsSortedByMaxSpent();
 
+        //Then:
         assertEquals(expectedClients.size(), clients.size());
         assertEquals(expectedClients.get(6).getId(), clients.get(5).getId());
         assertEquals(expectedClients.get(4).getId(), clients.get(6).getId());
@@ -81,9 +99,15 @@ public class ClientServiceTest {
 
     @Test
     void getClientsSortedByMaxSpentShouldReturnSortedClientsByMaxSpent() {
-        List<Client> expectedClients = clientCreator.getFakeClients();
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClients());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchases());
+        List<Client> expectedClients = getFakeClients();
+
+        //When:
         List<Client> clients = clientService.getClientsSortedByMaxSpent();
 
+        //Then:
         assertEquals(expectedClients.size(), clients.size());
         assertEquals(expectedClients.get(1).getId(), clients.get(0).getId());
         assertEquals(expectedClients.get(5).getId(), clients.get(1).getId());
@@ -94,90 +118,133 @@ public class ClientServiceTest {
 
     @Test
     void getClientWithMaxBuyInYearShouldThrowExceptionIfNotExistClient() {
-        when(mockedClientRepository.getAll()).thenReturn(clientCreator.getFakeClientsEmpty());
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClientsEmpty());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchases());
         String year = "2016";
 
+        //When:
         Throwable ex = assertThrows(NoSuchElementException.class, () -> {
             Client client = clientService.getClientWithMaxBuyInYear(year);
         });
 
+        //Then:
         assertEquals("There are no client or purchase in" + year, ex.getMessage());
     }
 
     @Test
     void getClientWithMaxBuyInYearShouldThrowExceptionIfNotExistPurchase() {
-        when(mockedPurchaseService.getAll()).thenReturn(purchaseCreator.getFakePurchasesEmpty());
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClients());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchasesEmpty());
         String year = "2016";
 
+        //When:
         Throwable ex = assertThrows(NoSuchElementException.class, () -> {
             Client client = clientService.getClientWithMaxBuyInYear(year);
         });
 
+        //Then:
         assertEquals("There are no client or purchase in" + year, ex.getMessage());
     }
 
     @Test
     void getClientWithMaxBuyInYearShouldThrowExceptionIfNotExistPurchaseInYear() {
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClients());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchases());
         String year = "1994";
 
+        //When:
         Throwable ex = assertThrows(NoSuchElementException.class, () -> {
             Client client = clientService.getClientWithMaxBuyInYear(year);
         });
 
+        //Then:
         assertEquals("There are no client or purchase in" + year, ex.getMessage());
     }
 
     @Test
     void getClientWithMaxBuyInYearShouldReturnClientWithMaxBuyInYear() {
-        int expectedClientId = clientCreator.getFakeClients().get(1).getId();
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClients());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchases());
+        int expectedClientId = getFakeClients().get(1).getId();
+
+        //When:
         int clientWithMaxBuyId = clientService.getClientWithMaxBuyInYear("2016").getId();
 
+        //Then:
         assertEquals(expectedClientId, clientWithMaxBuyId);
     }
 
     @Test
     void getLoyalClientsShouldReturnNoClientIfNotExistClient() {
-        when(mockedClientRepository.getAll()).thenReturn(clientCreator.getFakeClientsEmpty());
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClientsEmpty());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchases());
+
+        //When:
         List<Client> clients = clientService.getLoyalClients();
 
+        //Then:
         assertTrue(clients.isEmpty());
     }
 
     @Test
     void getLoyalClientsShouldReturnNoClientIfNotExistPurchase() {
-        when(mockedPurchaseService.getAll()).thenReturn(purchaseCreator.getFakePurchasesEmpty());
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClients());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchasesEmpty());
+
+        //When:
         List<Client> clients = clientService.getLoyalClients();
 
+        //Then:
         assertTrue(clients.isEmpty());
     }
 
     @Test
     void getLoyalClientsShouldReturnLoyalClients() {
-        List<Client> fakeClients = clientCreator.getFakeClients();
+        //Given:
+        when(mockedClientRepository.getAll()).thenReturn(getFakeClients());
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchases());
+        List<Client> expectedClients = getFakeClients();
+
+        //When:
         List<Client> loyalClients = clientService.getLoyalClients();
 
+        //Then:
         assertEquals(2, loyalClients.size());
-        assertEquals(fakeClients.get(1).getId(), loyalClients.get(0).getId());
-        assertEquals(fakeClients.get(2).getId(), loyalClients.get(1).getId());
+        assertEquals(expectedClients.get(1).getId(), loyalClients.get(0).getId());
+        assertEquals(expectedClients.get(2).getId(), loyalClients.get(1).getId());
     }
 
     @Test
     void getRecommendedWineShouldThrowExceptionIfClientHasNoPurchase() {
+        //Given:
+        when(mockedPurchaseService.getAll()).thenReturn(getFakePurchases());
+
+        //When:
         Throwable ex = assertThrows(NoSuchElementException.class, () -> {
             Wine wine = clientService.getRecommendedWine("000.000.000-08");
         });
 
+        //Then:
         assertEquals("This client haven't purchase yet!", ex.getMessage());
     }
 
     @Test
     void getRecommendedWineShouldReturnRecommendedWine() {
+        //Given:
         String fakeCPF = "000.000.000-02";
-        when(mockedPurchaseService.getClientPurchases(fakeCPF)).thenReturn(purchaseCreator.getPurchasesOfClient(fakeCPF));
-
-        Wine wine = clientService.getRecommendedWine(fakeCPF);
         Wine expectedWine = new WineCreator().getFakeWines().get(0);
+        when(mockedPurchaseService.getClientPurchases(fakeCPF)).thenReturn(getPurchasesOfClient(fakeCPF));
 
+        //When:
+        Wine wine = clientService.getRecommendedWine(fakeCPF);
+
+        //Then:
         assertEquals(expectedWine.getCodigo(), wine.getCodigo());
     }
 }

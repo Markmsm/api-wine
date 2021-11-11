19f5ecfe89054304cfbe00ab25ad6c446af88938
Mark <marksmunari@gmail.com>
2021-11-11 08:24:45 -0300

    refactor PurchaseServiceTest

--- src/test/java/br/com/wine/apiwine/service/PurchaseServiceTest.java
+++ src/test/java/br/com/wine/apiwine/service/PurchaseServiceTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import static br.com.wine.apiwine.data.model.PurchaseCreator.getFakePurchases;
+import static br.com.wine.apiwine.data.model.PurchaseCreator.getFakePurchasesEmpty;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -18,57 +20,75 @@ public class PurchaseServiceTest {
 
     static PurchaseRepository mockedPurchaseRepository;
     static PurchaseService purchaseService;
-    static PurchaseCreator purchaseCreator;
 
     @BeforeEach
     void setUp() {
         mockedPurchaseRepository = mock(PurchaseRepository.class);
         purchaseService = new PurchaseService(mockedPurchaseRepository);
-        purchaseCreator = new PurchaseCreator();
-
-        when(mockedPurchaseRepository.getAll()).thenReturn(purchaseCreator.getFakePurchases());
     }
 
     @Test
     void getAllShouldThrowExceptionIfNoPurchaseExist() {
-        when(mockedPurchaseRepository.getAll()).thenReturn(purchaseCreator.getFakePurchasesEmpty());
+        //Given:
+        when(mockedPurchaseRepository.getAll()).thenReturn(getFakePurchasesEmpty());
+
+        //When:
         Throwable ex = assertThrows(NoSuchElementException.class, () -> {
             List<Purchase> purchases = purchaseService.getAll();
         });
 
+        //Then:
         assertEquals("There is no purchase!", ex.getMessage());
     }
 
     @Test
     void getAllShouldReturnAllPurchases() {
-        List<Purchase> expectedPurchases = purchaseCreator.getFakePurchases();
+        //Given:
+        when(mockedPurchaseRepository.getAll()).thenReturn(getFakePurchases());
+        List<Purchase> expectedPurchases = getFakePurchases();
+
+        //When:
         List<Purchase> purchases = purchaseService.getAll();
 
+        //Then:
         assertEquals(expectedPurchases.size(), purchases.size());
         assertTrue(purchases.containsAll(expectedPurchases));
     }
 
     @Test
     void getClientPurchasesShouldReturnEmptyListIfNoPurchaseExist() {
-        when(mockedPurchaseRepository.getAll()).thenReturn(purchaseCreator.getFakePurchasesEmpty());
+        //Given:
+        when(mockedPurchaseRepository.getAll()).thenReturn(getFakePurchasesEmpty());
 
+        //When:
         List<Purchase> purchases = purchaseService.getClientPurchases("000.000.000-08");
 
+        //Then:
         assertTrue(purchases.isEmpty());
     }
 
     @Test
     void getClientPurchasesShouldReturnEmptyListIfNoPurchaseExistFromClient() {
+        //Given:
+        when(mockedPurchaseRepository.getAll()).thenReturn(getFakePurchases());
+
+        //When:
         List<Purchase> purchases = purchaseService.getClientPurchases("000.000.000-08");
 
+        //Then:
         assertTrue(purchases.isEmpty());
     }
 
     @Test
     void getClientPurchasesShouldReturnPurchasesFromClient() {
+        //Given:
+        when(mockedPurchaseRepository.getAll()).thenReturn(getFakePurchases());
         String clientCpf = "000.000.000.01";
+
+        //When:
         List<Purchase> purchases = purchaseService.getClientPurchases(clientCpf);
 
+        //Then:
         assertEquals(2, purchases.size());
         assertEquals(clientCpf, purchases.get(0).getCliente());
         assertEquals(clientCpf, purchases.get(1).getCliente());
